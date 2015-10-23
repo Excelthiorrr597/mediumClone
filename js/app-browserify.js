ede3501842d15c3f4aa9637a6b4ddf79f5c0ca9d
Jonathon Gregory <JGregory@Jonathons-MacBook-Pro.local>
2015-10-22 19:04:27 -0500

    added view for all users and single user view

--- js/app-browserify.js
+++ js/app-browserify.js
@@ -14,6 +14,35 @@ var APP_ID = 'KC3i5dxIw2AfhGrolMx0mspMpueAGB5WqOdijqzZ',
 	REST_API = 'lKahplMmgvTTiO1NAy0PPGmqC5ppKgtmkLtuKyFp'
 
 Parse.initialize(APP_ID,JS_KEY)
+window.p = Parse
+
+//=============Collecions and Models=============//
+
+var PostModel = Backbone.Model.extend({
+	url: "https://api.parse.com/1/classes/Posts",
+
+	parseHeaders: {
+		"X-Parse-Application-Id": APP_ID,
+		"X-Parse-REST-API-Key": REST_API
+	}
+})
+
+var PostCollection = Backbone.Collection.extend({
+	url: "https://api.parse.com/1/classes/Posts",
+
+	parseHeaders: {
+		"X-Parse-Application-Id": APP_ID,
+		"X-Parse-REST-API-Key": REST_API
+	},
+
+	model: PostModel,
+
+	parse: function(responseData) {
+		return responseData.results
+	}
+})
+
+//==============SIGN UP PAGE===============//
 
 var SignInView = React.createClass({
 
@@ -74,11 +103,213 @@ var LogInView = React.createClass({
 	}
 })
 
+//=================HOME PAGE=================//
+
+var HomeView = React.createClass({
+
+	componentDidMount: function(){
+		var self = this
+		this.props.collection.on('sync change', 
+			function(){
+				console.log(self.props.collection)
+				self.forceUpdate()
+			})
+	},
+
+	componentWillUnmount: function(){
+		var self = this
+		this.props.collection.off('sync change', 
+			function(){
+				self.forceUpdate()
+			})
+	},
+
+	_handleEnter: function(event){
+		if (event.which === 13) {
+			event.preventDefault()
+			var message = event.target.innerHTML
+			event.target.innerHTML = ''
+			this.props.deliverMessage(message)
+		}
+	},
+
+	_logOut:function(){
+		location.hash = 'logOut'
+	},
+
+	_showUserPosts: function(){
+		location.hash = 'user'
+	},
+
+	render: function(){
+		var style = {
+			'backgroundColor':'white'
+		},
+		style2 = {
+			'backgroundColor':'white',
+			'marginBottom':'20px'
+		}
+
+		return (
+			<div>
+				<div id="viewButtons">
+					<Button onClick={this._logOut} label='Log Me Out!' style={style}/>
+				</div>
+				<div 
+					id="messageContainer" 
+					contentEditable='true' 
+					onKeyPress={this._handleEnter}
+					>
+				</div>
+				<div id="viewButtons">
+					<Button onClick={this._showUserPosts} label='View Your Posts!' style={style2}/>
+				</div>
+				<PostBox collection={this.props.collection}/>
+			</div>
+			)
+	}
+})
+
+var PostBox = React.createClass({
+
+	_genPosts: function(postObj) {
+		return (<Posts key={postObj.id} model={postObj}/>)
+	},
+
+	render: function() {
+		return (
+			<div id="postContainer">
+				{this.props.collection.map(this._genPosts)}
+			</div>
+			)
+	}
+})
+
+var Posts = React.createClass({
+
+	render: function(){
+		var message = this.props.model.attributes.content
+
+		return (
+			<div id="individualPosts">
+				{message}
+			</div>
+			)
+	}
+})
+
+//=================USER VIEW================//
+
+var UserView = React.createClass({
+
+	componentDidMount: function(){
+		var self = this
+		this.props.collection.on('sync change', 
+			function(){
+				console.log(self.props.collection)
+				self.forceUpdate()
+			})
+	},
+
+	componentWillUnmount: function(){
+		var self = this
+		this.props.collection.off('sync change', 
+			function(){
+				self.forceUpdate()
+			})
+	},
+
+	_handleEnter: function(event){
+		if (event.which === 13) {
+			event.preventDefault()
+			var message = event.target.innerHTML
+			event.target.innerHTML = ''
+			this.props.deliverMessage(message)
+		}
+	},
+
+	_goHome:function(){
+		location.hash = 'home'
+	},
+
+	render: function(){
+		var style = {
+			'backgroundColor':'white'
+		}
+
+		return (
+			<div>
+				<div id="viewButtons">
+					<Button onClick={this._goHome} label='Go Back Home!' style={style}/>
+				</div>
+				<div 
+					id="messageContainer" 
+					contentEditable='true' 
+					onKeyPress={this._handleEnter}
+					>
+				</div>
+				<PostBox collection={this.props.collection}/>
+			</div>
+			)
+	}
+})
+
+// var PostBox = React.createClass({
+
+// 	_genPosts: function(postObj) {
+// 		return (<Posts key={postObj.id} model={postObj}/>)
+// 	},
+
+// 	render: function() {
+// 		return (
+// 			<div id="postContainer">
+// 				{this.props.collection.map(this._genPosts)}
+// 			</div>
+// 			)
+// 	}
+// })
+
+// var Posts = React.createClass({
+
+// 	render: function(){
+// 		var message = this.props.model.attributes.content
+
+// 		return (
+// 			<div id="individualPosts">
+// 				{message}
+// 			</div>
+// 			)
+// 	}
+// })
+
+//===================ROUTER==================//
+
 var MediumRouter = Backbone.Router.extend({
 
 	routes: {
+		'home': 'showHomeView',
+		'logOut': 'logOut',
 		'login': 'showSignInView',
-		'*anyroute': 'showSignInView'
+		'user': 'showUserView'
+	},
+
+	deliverMessage: function(message){
+		var pm = new PostModel()
+		pm.set({
+			content: message,
+			userid: Parse.User.current().id
+		})
+		this.pc.add(pm)
+		pm.save(null,{
+			headers: pm.parseHeaders
+		})
+	},
+
+	logOut: function(){
+		Parse.User.logOut().then(function(){
+			location.hash = 'login'
+		})
+		this.pc.reset()
 	},
 
 	processLogInInfo: function(username,password){
@@ -86,32 +317,58 @@ var MediumRouter = Backbone.Router.extend({
 		newUser.set('username',username)
 		newUser.set('password',password)
 		newUser.logIn().then(function(){
-			console.log('successfully logged in')
+			location.hash = 'home'
 		})
-
 	},
 
 	processSignUpInfo: function(username,password){
 		var newUser = new Parse.User()
 		newUser.set('username',username)
 		newUser.set('password',password)
+		console.log(username)
 		newUser.signUp().then(function(){
-			console.log('successfully signed up')
+			location.hash = 'home'
 		})
 	},
 
+	showHomeView: function() {
+		console.log('showing home view')
+		this.pc.fetch({
+			headers: this.pc.parseHeaders,
+			processData: true,
+		})
+		React.render(<HomeView deliverMessage={this.deliverMessage.bind(this)} collection={this.pc}/>,document.querySelector('#container'))
+	},
+
 	showSignInView: function(){
-		React.render(<SignInView sendSignUp={this.processSignUpInfo} sendLogIn={this.processLogInInfo}/>, document.querySelector('#container'))
+		React.render(<SignInView sendSignUp={this.processSignUpInfo} sendLogIn={this.processLogInInfo}/>,document.querySelector('#container'))
+	},
+
+	showUserView: function() {
+		console.log('showing user view')
+		var paramObj = {
+			userid: Parse.User.current().id
+		}
+		var stringifiedParams = JSON.stringify(paramObj)
+
+		this.pc.fetch({
+			headers: this.pc.parseHeaders,
+			processData: true,
+			data: {
+				where: stringifiedParams
+			}
+		})
+		React.render(<UserView deliverMessage={this.deliverMessage.bind(this)} collection={this.pc}/>,document.querySelector('#container'))
 	},
 
 	initialize:function(){
 		console.log('initialized router')
+		this.pc = new PostCollection()
 		if (!Parse.User.current()) {
 			location.hash = 'login'
 		}
 	}
 })
-
 
 var mdRtr = new MediumRouter()
 Backbone.history.start()
